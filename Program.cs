341eb4812e0228cce5034ce3530c6d2d64a4d3fe
PedroOliveira0609 <90793896+PedroOliveira0609@users.noreply.github.com>
2021-11-28 20:35:21 -0300

    contains

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Contains
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s1 = "O segredo é ter fé em Deus.";
+            string s2 = "a";
+            string s3 = "t";
+
+            Console.WriteLine(":{0} ", s1.Contains(s2));
+
+            Console.WriteLine(":{0} ", s1.Contains(s3));
+        }
+    }
+}
